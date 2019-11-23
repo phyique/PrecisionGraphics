477196549f06aca65763a3298698d05b9a510c8a
vaun-pierre wynter <vaun-pierre@hotmail.com>
2019-11-23 14:11:15 -0500

    added/updated main page header/footer

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,25 @@
-var express = require('express');
-var router = express.Router();
-var Router = [];
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+/* GET all pages. */
+router.get('/', (req, res) => {
   res.render('index', { title: 'Precision Graphics' });
+});
+
+router.get('/main', (req, res) => {
+  res.render('main', {title: 'Page Headers'})
+});
+
+router.get('/about', (req, res) => {
+  res.render('about', {title: 'About Us'})
+});
+
+router.get('/career', (req, res) => {
+  res.render('career', {title: 'Careers'})
+});
+
+router.get('/contact', (req, res) => {
+  res.render('contact', { title: 'About Us' });
 });
 
 module.exports = router;
